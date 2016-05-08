5e573fd12165176d246eb628a03c9212036b814c
Sindre Sorhus <sindresorhus@gmail.com>
2016-05-08 18:02:49 +0700

    minor test tweaks

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 import path from 'path';
-import fs from 'fs';
 import test from 'ava';
 import JSFtpRaw from 'jsftp';
 import Server from 'ftp-test-server';
 import pathExists from 'path-exists';
 import delay from 'delay';
+import del from 'del';
 import m from './';
 
 const JSFtp = m(JSFtpRaw);
@@ -36,20 +36,18 @@ test.before(async () => {
 
 test.after(() => {
 	mockServer.stop();
-	fs.rmdirSync(path.join(__dirname, 'foo', 'bar', 'baz'));
-	fs.rmdirSync(path.join(__dirname, 'foo', 'bar'));
-	fs.rmdirSync(path.join(__dirname, 'foo'));
+	del.sync('foo');
 });
 
-test('decorate JSFtp', t => {
+test.serial('decorate JSFtp', t => {
 	t.is(typeof ftp.mkdirp, 'function');
 });
 
-test('fail if no path is provided', t => {
+test.serial('fail if no path is provided', t => {
 	t.throws(ftp.mkdirp(), '`path` is required');
 });
 
-test('create nested directories', async t => {
+test.serial('create nested directories', async t => {
 	await ftp.mkdirp(testDir);
 	t.true(await pathExists(path.join(__dirname, testDir)));
 });
